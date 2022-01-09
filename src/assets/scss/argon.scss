f63c9bbe01b3b46d86a75919c9a7392c4cfef2f0
mukund <mukund.s@intelaccounts.com>
2022-01-09 20:27:18 +0530

    npm i

--- src/assets/scss/argon.scss
+++ src/assets/scss/argon.scss
@@ -4,11 +4,11 @@
 * Argon Design System Angular - v1.2.0
 =========================================================
 
-* Product Page: https://www.creative-tim.com/product/argon-design-system-angular
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/argon-design-system-angular/blob/master/LICENSE.md)
+* Product Page: https://www.unsportsmama.com/product/argon-design-system-angular
+* Copyright 2021 UnSports Mama (https://www.unsportsmama.com?)
+* Licensed under MIT (https://github.com/mukund720/unSportsMama/blob/master/LICENSE.md)
 
-* Coded by Creative Tim
+* Coded by UnSports Mama
 
 =========================================================
 
